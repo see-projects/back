66cf822a15db99f78ea9c0b7d08238ef5001cbe4
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-04 21:00:41 +0900

    프로젝트 기본 세팅 및 생성

---
+++ src/main/java/dooya/see/SeeApplication.java
@@ -0,0 +1,13 @@
+package dooya.see;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dooya/see/SeeApplicationTests.java
@@ -0,0 +1,13 @@
+package dooya.see;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
