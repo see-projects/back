d1c7f82b1d2505ec1d602ea805ebc614bf26def7
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-13 22:03:54 +0900

    userSignUp 메서드 구현중 회원가입을 처리할 userSignUpService, 응답으로 보여줄 Response DTO를 선언, 아직은 에러 표시

--- src/main/java/dooya/see/user/presentation/UserController.java
+++ src/main/java/dooya/see/user/presentation/UserController.java
@@ -1,4 +1,17 @@
 package dooya.see.user.presentation;
 
+import dooya.see.user.application.UserSignUpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @PostMapping
+    public ResponseEntity<UserSignUpResponse> userSignUp(UserSignUpRequest request) {
+        return ResponseEntity.created(userSignUpService.signUp(request));
+    }
 }
