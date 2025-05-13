941d0b01886fdf2f9bdf9c8246004a7ecf76019f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-13 19:29:17 +0900

    E2E 테스트를 위한 SpringBootTest, AutoConfigureMockMvc 설정 및 의존성 주입

--- src/test/java/dooya/see/user/presentation/UserControllerTest.java
+++ src/test/java/dooya/see/user/presentation/UserControllerTest.java
@@ -1,4 +1,25 @@
 package dooya.see.user.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("유저 회원가입 성공 테스트")
+    @Test
+    void user_signUp_success() {
+    }
 }
