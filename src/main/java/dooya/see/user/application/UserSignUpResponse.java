ca35ae846ba6d7d13efed4c167e558611d998641
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-14 00:11:07 +0900

    UserSignUpResponse에서 user 데이터 반환하도록 변경

--- src/main/java/dooya/see/user/application/UserSignUpResponse.java
+++ src/main/java/dooya/see/user/application/UserSignUpResponse.java
@@ -1,6 +1,7 @@
 package dooya.see.user.application;
 
 import dooya.see.user.domain.Role;
+import dooya.see.user.domain.User;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -14,4 +15,15 @@ public record UserSignUpResponse(
         String phoneNumber,
         Role role
 ) {
+
+    public static UserSignUpResponse from(User user){
+        return UserSignUpResponse.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .birthDate(user.getBirthDate())
+                .phoneNumber(user.getPhoneNumber())
+                .role(user.getRole())
+                .build();
+    }
 }
