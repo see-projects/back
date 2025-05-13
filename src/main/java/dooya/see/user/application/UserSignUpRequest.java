7c9cbe5a3b7f82bf42762e443ecaf5d15b2c1179
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-14 00:10:36 +0900

    UserSignUpRequest Builder 제거

--- src/main/java/dooya/see/user/application/UserSignUpRequest.java
+++ src/main/java/dooya/see/user/application/UserSignUpRequest.java
@@ -1,10 +1,7 @@
 package dooya.see.user.application;
 
-import lombok.Builder;
-
 import java.time.LocalDate;
 
-@Builder
 public record UserSignUpRequest(
         String email,
         String name,
