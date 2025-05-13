f096862ff3bf4ce4d9e00a54dc8a2e48a79362a5
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-13 22:32:56 +0900

    UserController에 UserSignUpService 주입 및 구현

--- src/main/java/dooya/see/user/presentation/UserController.java
+++ src/main/java/dooya/see/user/presentation/UserController.java
@@ -1,17 +1,27 @@
 package dooya.see.user.presentation;
 
 import dooya.see.user.application.UserSignUpRequest;
+import dooya.see.user.application.UserSignUpResponse;
+import dooya.see.user.application.UserSignUpService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserSignUpService userSignUpService;
 
     @PostMapping
     public ResponseEntity<UserSignUpResponse> userSignUp(UserSignUpRequest request) {
-        return ResponseEntity.created(userSignUpService.signUp(request));
+        UserSignUpResponse response = userSignUpService.userSignUp(request);
+        URI location = URI.create("/api/users/" + response.id());
+        return ResponseEntity.created(location).body(response);
     }
 }
