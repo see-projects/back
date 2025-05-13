6d7086b5084189af4f5d6ea2b8d257136e0aa89f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-13 22:43:05 +0900

    User 도메인 생성

---
+++ src/main/java/dooya/see/user/domain/User.java
@@ -0,0 +1,33 @@
+package dooya.see.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private LocalDate birthDate;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
