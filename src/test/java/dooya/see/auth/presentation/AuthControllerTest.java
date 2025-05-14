148c93a02e1b6ac7e8792e79b34eebfd95376902
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-14 20:10:57 +0900

    AuthControllerTest 생성 및 MockMvc, ObjectMapper 주입

---
+++ src/test/java/dooya/see/auth/presentation/AuthControllerTest.java
@@ -0,0 +1,20 @@
+package dooya.see.auth.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
