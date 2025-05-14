5559d941efca3c475a566ec553ec0babb407b8f9
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-14 22:43:57 +0900

    auth 경로 인가 허용

--- src/main/java/dooya/see/auth/config/SecurityConfig.java
+++ src/main/java/dooya/see/auth/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
         // 경로별 인가 작업
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/api/users/**").permitAll()
+                .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/webjars/**").permitAll()
                 .anyRequest().authenticated());
 
