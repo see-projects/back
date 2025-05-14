b8f29c903ee20a20369ff60ac87e1f7bb9df9686
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-15 00:24:13 +0900

    AuthValidator 구현체 생성

---
+++ src/main/java/dooya/see/auth/application/AuthValidatorImpl.java
@@ -0,0 +1,24 @@
+package dooya.see.auth.application;
+
+import dooya.see.common.exception.CustomException;
+import dooya.see.common.exception.ErrorCode;
+import dooya.see.user.domain.User;
+import dooya.see.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthValidatorImpl implements AuthValidator {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public User validateEmailAndPassword(String email, String password) {
+        return userRepository.findByEmail(email)
+                .filter(user -> passwordEncoder.matches(password, user.getPassword()))
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_MATCH_LOGIN_INFO));
+    }
+}
