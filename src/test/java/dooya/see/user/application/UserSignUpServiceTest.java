75e44785f69284cd321052a0b58bdd50d4121089
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-14 15:38:38 +0900

    단위테스트 항목 추가 및 UserValidator 주입 및 테스트 리팩터링

--- src/test/java/dooya/see/user/application/UserSignUpServiceTest.java
+++ src/test/java/dooya/see/user/application/UserSignUpServiceTest.java
@@ -1,24 +1,25 @@
 package dooya.see.user.application;
 
+import dooya.see.common.exception.CustomException;
+import dooya.see.common.exception.ErrorCode;
 import dooya.see.user.domain.Role;
 import dooya.see.user.domain.User;
 import dooya.see.user.domain.UserRepository;
+import dooya.see.user.fixture.UserSignUpFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserSignUpServiceTest {
@@ -29,16 +30,17 @@ public class UserSignUpServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private UserValidator userValidator;
+
     @DisplayName("유저 회원가입 성공 단위테스트")
     @Test
     void user_signUp_success() {
         // Arrange
-        UserSignUpRequest request = new UserSignUpRequest("test@see.com", "testName", "testPassword", LocalDate.of(2001, 1, 4), "01012345678");
+        UserSignUpRequest request = UserSignUpFixture.request();
+        User testUser = UserSignUpFixture.createTestUser(request);
 
-        given(userRepository.findByEmail(request.email())).willReturn(java.util.Optional.empty());
-
-        User testUser = User.signUpUser(request.email(), request.name(), request.password(), request.birthDate(), request.phoneNumber(), Role.of("USER"));
-        ReflectionTestUtils.setField(testUser, "id", 1L);
+        doNothing().when(userValidator).validateDuplicateEmail(request.email());
         given(userRepository.save(any(User.class))).willReturn(testUser);
 
         // Act
@@ -55,7 +57,21 @@ public class UserSignUpServiceTest {
                 () -> assertThat(response.role()).isEqualTo(Role.USER)
         );
 
-        then(userRepository).should(times(1)).findByEmail(request.email());
+        then(userValidator).should(times(1)).validateDuplicateEmail(request.email());
         then(userRepository).should(times(1)).save(any(User.class));
+    }
+
+    @DisplayName("유저 회원가입 실패 단위테스트 - 중복되는 이메일")
+    @Test
+    void user_signUp_fail() {
+        // Arrange
+        UserSignUpRequest request = UserSignUpFixture.request();
+        doThrow(new CustomException(ErrorCode.USER_ALREADY_EXISTS)).when(userValidator).validateDuplicateEmail(request.email());
+
+        // Act && Assert
+        assertThrows(CustomException.class, () -> userSignUpService.userSignUp(request));
+
+        then(userValidator).should(times(1)).validateDuplicateEmail(request.email());
+        then(userRepository).shouldHaveNoInteractions();
     }
 }
