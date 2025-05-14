ed38e4996396e6431db8fdbc28b7557cf0e29342
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-15 00:24:43 +0900

    AuthFixture 수정

--- src/test/java/dooya/see/common/AuthFixture.java
+++ src/test/java/dooya/see/common/AuthFixture.java
@@ -5,6 +5,6 @@ import dooya.see.auth.application.LoginRequest;
 public class AuthFixture {
 
     public static LoginRequest request() {
-        return new LoginRequest("test@see.com", "testName");
+        return new LoginRequest("test@see.com", "testPassword");
     }
 }
