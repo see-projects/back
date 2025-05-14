e0111175d3ce21c940786254f312a469227e7a55
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-14 15:36:50 +0900

    User가 이미 존재할때는 예외를 발생시키는 Validator 컴포넌트 생성

---
+++ src/main/java/dooya/see/user/application/UserValidatorImpl.java
@@ -0,0 +1,26 @@
+package dooya.see.user.application;
+
+import dooya.see.common.exception.CustomException;
+import dooya.see.common.exception.ErrorCode;
+import dooya.see.user.domain.User;
+import dooya.see.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@RequiredArgsConstructor
+public class UserValidatorImpl implements UserValidator {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void validateDuplicateEmail(String email) {
+        userRepository.findByEmail(email)
+                .ifPresent(UserValidatorImpl::throwUserAlreadyExists);
+    }
+
+    private static void throwUserAlreadyExists(User user) {
+        throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
+    }
+}
