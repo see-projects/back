77d612b4f9b8fba655b10f47c39c0dcbdd0fae10
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-14 15:37:50 +0900

    Request 유효성 검사를 위해 @NotBlank @Email 어노테이션 추가, 테스트에서 사용하기 위해 @Builder 추가

--- src/main/java/dooya/see/user/application/UserSignUpRequest.java
+++ src/main/java/dooya/see/user/application/UserSignUpRequest.java
@@ -1,12 +1,27 @@
 package dooya.see.user.application;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
 import java.time.LocalDate;
 
+@Builder(toBuilder = true)
 public record UserSignUpRequest(
+        @NotBlank(message = "이메일은 비어 있을 수 없습니다")
+        @Email
         String email,
+
+        @NotBlank(message = "이름은 비어 있을 수 없습니다")
         String name,
+
+        @NotBlank(message = "비밀번호는 비어 있을 수 없습니다")
         String password,
+
+        @NotBlank(message = "생년월일은 비어 있을 수 없습니다")
         LocalDate birthDate,
+
+        @NotBlank(message = "전화번호는 비어 있을 수 없습니다")
         String phoneNumber
 ) {
 }
