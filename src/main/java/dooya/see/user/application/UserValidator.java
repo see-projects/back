c5f77f6ccad86a6bb0bc236592c4e7bb5e834036
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-17 21:43:43 +0900

    UserValidator에 Email 검증로직 추가

--- src/main/java/dooya/see/user/application/UserValidator.java
+++ src/main/java/dooya/see/user/application/UserValidator.java
@@ -1,5 +1,8 @@
 package dooya.see.user.application;
 
+import dooya.see.user.domain.User;
+
 public interface UserValidator {
     void validateDuplicateEmail(String email);
+    User getUserFromToken(String email);
 }
