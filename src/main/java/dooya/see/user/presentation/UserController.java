1d79e68cec864c3dff2f46e48c4437c6944061f2
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-17 21:44:57 +0900

    UserController에 토큰으로 조회 로직 추가

--- src/main/java/dooya/see/user/presentation/UserController.java
+++ src/main/java/dooya/see/user/presentation/UserController.java
@@ -1,15 +1,15 @@
 package dooya.see.user.presentation;
 
+import dooya.see.auth.domain.LoginUser;
+import dooya.see.user.application.UserQueryService;
 import dooya.see.user.application.UserSignUpCommand;
 import dooya.see.user.application.UserSignUpService;
 import dooya.see.user.domain.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -19,6 +19,7 @@ import java.net.URI;
 public class UserController {
 
     private final UserSignUpService userSignUpService;
+    private final UserQueryService userQueryService;
 
     @PostMapping
     public ResponseEntity<UserSignUpResponse> userSignUp(@Valid @RequestBody UserSignUpRequest request) {
@@ -27,5 +28,14 @@ public class UserController {
         UserSignUpResponse response = UserDtoMapper.toResponse(user);
 
         return ResponseEntity.created(URI.create("/api/users/" + user.getId())).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<UserSignUpResponse> getUserByToken(@AuthenticationPrincipal LoginUser loginUser) {
+        String email = loginUser.getUsername();
+        User user = userQueryService.getUserFromToken(email);
+        UserSignUpResponse response = UserDtoMapper.toResponse(user);
+
+        return ResponseEntity.ok().body(response);
     }
 }
