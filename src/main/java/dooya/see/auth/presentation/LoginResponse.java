8024c90e0207eaf06e49d7c44bb7280298a0ca9d
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-19 13:37:50 +0900

    LoginResponse 필드 수정

--- src/main/java/dooya/see/auth/presentation/LoginResponse.java
+++ src/main/java/dooya/see/auth/presentation/LoginResponse.java
@@ -2,15 +2,12 @@ package dooya.see.auth.presentation;
 
 import lombok.Builder;
 
-import java.time.LocalDate;
-
 @Builder
 public record LoginResponse(
         String accessToken,
         Long id,
         String email,
         String name,
-        LocalDate birthDate,
-        String phoneNumber
+        String nickName
 ) {
 }
