23da4162aa570d229acc81b5d9749ce7e9c67691
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-19 17:16:49 +0900

    import 수정

--- src/main/java/dooya/see/user/application/UserResult.java
+++ src/main/java/dooya/see/user/application/UserResult.java
@@ -1,10 +1,12 @@
 package dooya.see.user.application;
 
+import dooya.see.user.domain.Role;
+
 public record UserResult(
         Long id,
         String email,
         String name,
         String nickName,
-        dooya.see.user.domain.Role role
+        Role role
 ) {
 }
