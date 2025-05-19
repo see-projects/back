eeccb0a4ebd996331ff03a5a490310e8cb6c79df
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-19 16:19:27 +0900

    테스트 수정

--- src/test/java/dooya/see/user/application/UserUpdateServiceTest.java
+++ src/test/java/dooya/see/user/application/UserUpdateServiceTest.java
@@ -2,14 +2,28 @@ package dooya.see.user.application;
 
 import dooya.see.common.UserFixture;
 import dooya.see.user.domain.User;
+import dooya.see.user.domain.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
+
+@ExtendWith(MockitoExtension.class)
 public class UserUpdateServiceTest {
 
-    private final UserUpdateServiceImpl userUpdateService = new UserUpdateServiceImpl();
+    @InjectMocks
+    private UserUpdateServiceImpl userUpdateService;
+
+    @Mock
+    private UserRepository userRepository;
 
     @DisplayName("유저 닉네임 업데이트 성공 테스트")
     @Test
@@ -18,10 +32,12 @@ public class UserUpdateServiceTest {
         User testUser = UserFixture.testUser();
         UserUpdateCommand command = UserFixture.updateCommand();
 
+        given(userRepository.findByEmail(testUser.getEmail())).willReturn(Optional.of(testUser));
+
         // Act
-        userUpdateService.updateNickName(testUser, command);
+        User user = userUpdateService.updateNickName(testUser.getEmail(), command);
 
         // Assert
-        assertThat(testUser.getNickName()).isEqualTo("updateNickName");
+        assertThat(user.getNickName()).isEqualTo(testUser.getNickName());
     }
 }
