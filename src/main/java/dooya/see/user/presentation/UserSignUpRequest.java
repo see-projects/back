7d3b1a2c7c97e628856f5ec6d9643204b95659ae
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-19 13:35:55 +0900

    UserSignUpRequest 필드 변경 -> 생년월일, 전화번호 삭제후 닉네임 추가

--- src/main/java/dooya/see/user/presentation/UserSignUpRequest.java
+++ src/main/java/dooya/see/user/presentation/UserSignUpRequest.java
@@ -1,12 +1,8 @@
 package dooya.see.user.presentation;
 
-import dooya.see.user.application.UserSignUpCommand;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-
-import java.time.LocalDate;
 
 @Builder(toBuilder = true)
 public record UserSignUpRequest(
@@ -20,10 +16,7 @@ public record UserSignUpRequest(
         @NotBlank(message = "비밀번호는 비어 있을 수 없습니다")
         String password,
 
-        @NotNull(message = "생년월일은 비어 있을 수 없습니다")
-        LocalDate birthDate,
-
-        @NotBlank(message = "전화번호는 비어 있을 수 없습니다")
-        String phoneNumber
+        @NotBlank(message = "닉네임은 비어 있을 수 없습니다")
+        String nickName
 ) {
 }
