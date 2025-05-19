f20e484814b57eb9c5fb278c6018cbaabc407128
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-19 15:41:18 +0900

    Mock 의존성이 없으므로 @Injectmocks 제거

--- src/test/java/dooya/see/user/application/UserUpdateServiceTest.java
+++ src/test/java/dooya/see/user/application/UserUpdateServiceTest.java
@@ -4,17 +4,12 @@ import dooya.see.common.UserFixture;
 import dooya.see.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
 public class UserUpdateServiceTest {
 
-    @InjectMocks
-    private UserUpdateServiceImpl userUpdateService;
+    private final UserUpdateServiceImpl userUpdateService = new UserUpdateServiceImpl();
 
     @DisplayName("유저 닉네임 업데이트 성공 테스트")
     @Test
