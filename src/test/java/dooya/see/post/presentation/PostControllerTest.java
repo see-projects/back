88e276845a6a911b87f5d54d9b523cd35034a1df
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-22 15:21:39 +0900

    /api/post 경로로 post 메서드 요청 테스트 작성 및 UserFixture 유저 저장, 테스트 전용 토큰 생성 로직 추가

--- src/test/java/dooya/see/post/presentation/PostControllerTest.java
+++ src/test/java/dooya/see/post/presentation/PostControllerTest.java
@@ -1,13 +1,23 @@
 package dooya.see.post.presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dooya.see.auth.util.JwtUtil;
+import dooya.see.common.UserFixture;
+import dooya.see.user.domain.User;
+import dooya.see.user.infrastructure.UserJpaRepository;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,9 +30,31 @@ public class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    private String testToken;
+
+    @BeforeEach
+    void setUp() {
+        User testUser = userJpaRepository.save(UserFixture.testUser());
+        testToken = jwtUtil.createAccessToken(testUser.getId(), testUser.getEmail(), testUser.getRole());
+    }
+
     @DisplayName("게시글 작성 성공 테스트")
     @Test
     void user_post_success() {
+        // Arrange
+        PostRequest request = PostFixture.request();
 
+        // Act && Assert
+        mockMvc.perform(post("/api/post")
+                        .cookie(new Cookie("Authorization", testToken))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
     }
 }
