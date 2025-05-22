a9b124651f4b0388ae7b34f6798a1151d556d2ee
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-22 21:35:04 +0900

    PostControllerTest 게시글 작성 성공 / 실패 테스트 작성

--- src/test/java/dooya/see/post/presentation/PostControllerTest.java
+++ src/test/java/dooya/see/post/presentation/PostControllerTest.java
@@ -1,17 +1,19 @@
 package dooya.see.post.presentation;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dooya.see.auth.util.JwtUtil;
 import dooya.see.common.PostFixture;
 import dooya.see.common.UserFixture;
-import dooya.see.post.application.PostRequest;
+import dooya.see.post.presentation.dto.PostRequest;
 import dooya.see.user.domain.User;
 import dooya.see.user.infrastructure.UserJpaRepository;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,7 +21,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.stream.Stream;
+
+import static dooya.see.common.PostFixture.*;
+import static dooya.see.common.UserFixture.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -40,10 +47,11 @@ public class PostControllerTest {
     private JwtUtil jwtUtil;
 
     private String testToken;
+    private User testUser;
 
     @BeforeEach
     void setUp() {
-        User testUser = userJpaRepository.save(UserFixture.testUser());
+        testUser = userJpaRepository.save(testUser());
         testToken = jwtUtil.createAccessToken(testUser.getId(), testUser.getEmail(), testUser.getRole());
     }
 
@@ -51,13 +59,46 @@ public class PostControllerTest {
     @Test
     void user_post_success() throws Exception {
         // Arrange
-        PostRequest request = PostFixture.request();
+        PostRequest request = request();
 
         // Act && Assert
         mockMvc.perform(post("/api/post")
                         .cookie(new Cookie("Authorization", testToken))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isOk());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.nickName").value(testUser.getNickName()))
+                .andExpect(jsonPath("$.title").value(request.title()))
+                .andExpect(jsonPath("$.content").value(request.content()));
     }
+
+    @DisplayName("게시글 작성 실패 테스트 - 개별 필드 유효성 검증")
+    @ParameterizedTest(name = "{index} => 필드 = {0}, 메시지 = {1}")
+    @MethodSource("invalidFieldProvider")
+    void post_fail_invalidField(PostRequest request, String field, String message) throws Exception {
+        mockMvc.perform(post("/api/post")
+                        .cookie(new Cookie("Authorization", testToken))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(false))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validationErrors[0].field").value(field))
+                .andExpect(jsonPath("$.validationErrors[0].message").value(message));
+    }
+
+    private static Stream<Arguments> invalidFieldProvider() {
+        return Stream.of(
+                Arguments.of(
+                        request().toBuilder().title("").build(),
+                        "title", "제목은 비어 있을 수 없습니다"
+                ),
+                Arguments.of(
+                        request().toBuilder().content("").build(),
+                        "content", "내용은 비어 있을 수 없습니다"
+                )
+        );
+    }
+
 }
