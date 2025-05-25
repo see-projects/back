d545a096340a2cb2fb968a5b1554ee4e9cf37cef
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-25 15:15:02 +0900

    Mapper 클래스에 변환 메서드 작성

--- src/main/java/dooya/see/post/application/dto/PostApplicationMapper.java
+++ src/main/java/dooya/see/post/application/dto/PostApplicationMapper.java
@@ -1,7 +1,12 @@
 package dooya.see.post.application.dto;
 
 import dooya.see.post.domain.Post;
+import dooya.see.user.application.dto.UserApplicationMapper;
+import dooya.see.user.application.dto.UserResult;
 import dooya.see.user.domain.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PostApplicationMapper {
 
@@ -20,5 +25,9 @@ public class PostApplicationMapper {
                 post.getTitle(),
                 post.getContent()
         );
+    }
+
+    public static List<PostResult> toResults(List<Post> posts) {
+        return posts.stream().map(PostApplicationMapper::toResult).collect(Collectors.toList());
     }
 }
