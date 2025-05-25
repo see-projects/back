69c054b6acca8a54f81ae04a9c40fd90dff28c7d
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-25 17:27:06 +0900

    AuthController에서 AuthService를 호출하는지 검증하는 슬라이스 테스트 작성 (AuthController 단위테스트)

--- src/test/java/dooya/see/auth/presentation/AuthControllerTest.java
+++ src/test/java/dooya/see/auth/presentation/AuthControllerTest.java
@@ -1,36 +1,29 @@
 package dooya.see.auth.presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dooya.see.auth.application.dto.LoginCommand;
+import dooya.see.auth.application.service.AuthService;
+import dooya.see.auth.config.SecurityConfig;
 import dooya.see.auth.presentation.dto.LoginRequest;
-import dooya.see.common.UserFixture;
-import dooya.see.user.infrastructure.UserJpaRepository;
-import org.junit.jupiter.api.BeforeEach;
+import dooya.see.auth.util.JwtUtil;
+import dooya.see.common.AuthFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.stream.Stream;
-
-import static dooya.see.common.AuthFixture.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(AuthController.class)
+@Import(SecurityConfig.class)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-@Sql("classpath:init.sql")
 public class AuthControllerTest {
 
     @Autowired
@@ -39,59 +32,28 @@ public class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    private UserJpaRepository userJpaRepository;
+    @MockitoBean
+    private AuthService authService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    @MockitoBean
+    private JwtUtil jwtUtil;
 
-    @BeforeEach
-    void setUp() {
-        userJpaRepository.save(UserFixture.mockUser(passwordEncoder));
-    }
-
-    @DisplayName("유저 로그인 성공 테스트")
+    @DisplayName("/login 경로로 POST 요청 시 authService.login(command) 호출 여부 검증")
     @Test
-    void user_login_success() throws Exception {
-        // Arrange
-        LoginRequest request = request();
+    void post_WhenCalled_InvokeLogin() throws Exception {
+        // given
+        LoginRequest request = AuthFixture.request();
+        LoginCommand command = AuthFixture.command();
 
-        // Act & Assert
+        given(authService.login(any(LoginCommand.class))).willReturn(AuthFixture.result());
+
+        // when
         mockMvc.perform(post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.accessToken").exists())
-                .andExpect(result -> {
-                    String cookie = result.getResponse().getHeader("Set-Cookie");
-                    assertThat(cookie).contains("Authorization");
-                });
-    }
+                .andExpect(status().isOk());
 
-    @DisplayName("유저 로그인 실패 테스트 - 개별 필드 유효성 검증")
-    @ParameterizedTest(name = "{index} => 필드 = {0}, 메시지 = {1}")
-    @MethodSource("invalidFieldProvider")
-    void user_login_fail_invalidField(LoginRequest request, String field, String message) throws Exception {
-        mockMvc.perform(post("/api/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status").value(false))
-                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
-                .andExpect(jsonPath("$.validationErrors[0].field").value(field))
-                .andExpect(jsonPath("$.validationErrors[0].message").value(message));
-    }
-
-    private static Stream<Arguments> invalidFieldProvider() {
-        return Stream.of(
-                Arguments.of(
-                        request().toBuilder().email("").build(),
-                        "email", "이메일은 비어 있을 수 없습니다"
-                ),
-                Arguments.of(
-                        request().toBuilder().password("").build(),
-                        "password", "비밀번호는 비어 있을 수 없습니다"
-                )
-        );
+        // then
+        then(authService).should().login(command);
     }
 }
