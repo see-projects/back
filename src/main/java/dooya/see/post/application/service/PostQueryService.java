70acf629fe31e0ba87cb6cadb3ce18bf8f7582e1
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-25 23:41:31 +0900

    PostQueryService 인터페이스 List -> Page로 수정 및 Pageable 객체 매개변수로 받도록 수정

--- src/main/java/dooya/see/post/application/service/PostQueryService.java
+++ src/main/java/dooya/see/post/application/service/PostQueryService.java
@@ -1,10 +1,12 @@
 package dooya.see.post.application.service;
 
 import dooya.see.post.application.dto.PostResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface PostQueryService {
-    List<PostResult> getPosts();
+    Page<PostResult> getPosts(Pageable pageable);
     PostResult getPost(Long id);
 }
