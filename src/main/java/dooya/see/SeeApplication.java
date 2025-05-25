f76d95dfea012637fcfa0995b8a79a6329b2d589
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-26 02:43:24 +0900

    JPA 변동사항 반영되게 어노테이션 추가

--- src/main/java/dooya/see/SeeApplication.java
+++ src/main/java/dooya/see/SeeApplication.java
@@ -2,8 +2,10 @@ package dooya.see;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SeeApplication {
 
     public static void main(String[] args) {
