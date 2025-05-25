360ed6bf07b9d2b04fa3827dfac8621feaa06bff
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-25 17:27:38 +0900

    AuthFixture에 LoginResult 객체 생성 및 MapperTest에 반영

--- src/test/java/dooya/see/auth/presentation/AuthPresentationMapperTest.java
+++ src/test/java/dooya/see/auth/presentation/AuthPresentationMapperTest.java
@@ -8,12 +8,12 @@ import dooya.see.common.UserFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static dooya.see.auth.presentation.dto.AuthDtoMapper.*;
+import static dooya.see.auth.presentation.dto.AuthPresentationMapper.*;
 import static dooya.see.common.AuthFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-public class AuthDtoMapperTest {
+public class AuthPresentationMapperTest {
 
     @DisplayName("LoginRequest를 LoginCommand로 정산 변환한다")
     @Test
@@ -35,7 +35,7 @@ public class AuthDtoMapperTest {
     @Test
     void convert_LoginResult_to_LoginResponse() {
         // Arrange
-        LoginResult result = new LoginResult(UserFixture.testUser(), "mocked-jwt-token");
+        LoginResult result = result();
 
         // Act
         LoginResponse response = toResponse(result);
@@ -43,10 +43,10 @@ public class AuthDtoMapperTest {
         // Assert
         assertAll(
                 () -> assertThat(response.accessToken()).isEqualTo("mocked-jwt-token"),
-                () -> assertThat(response.id()).isEqualTo(result.user().getId()),
-                () -> assertThat(response.email()).isEqualTo(result.user().getEmail()),
-                () -> assertThat(response.name()).isEqualTo(result.user().getName()),
-                () -> assertThat(response.nickName()).isEqualTo(result.user().getNickName())
+                () -> assertThat(response.id()).isEqualTo(result.id()),
+                () -> assertThat(response.email()).isEqualTo(result.email()),
+                () -> assertThat(response.name()).isEqualTo(result.name()),
+                () -> assertThat(response.nickName()).isEqualTo(result.nickName())
         );
     }
 }
--- src/test/java/dooya/see/common/AuthFixture.java
+++ src/test/java/dooya/see/common/AuthFixture.java
@@ -1,6 +1,7 @@
 package dooya.see.common;
 
 import dooya.see.auth.application.dto.LoginCommand;
+import dooya.see.auth.application.dto.LoginResult;
 import dooya.see.auth.presentation.dto.LoginRequest;
 
 public class AuthFixture {
@@ -11,5 +12,9 @@ public class AuthFixture {
 
     public static LoginCommand command() {
         return new LoginCommand("test@see.com", "testPassword");
+    }
+
+    public static LoginResult result() {
+        return new LoginResult(1L, "test@see.com", "testName", "testNickName", "mocked-jwt-token");
     }
 }
