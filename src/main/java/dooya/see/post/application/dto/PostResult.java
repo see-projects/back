533a1def1d4d35ba1741d950b1070d58a03f1f25
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-26 20:13:57 +0900

    응답 DTO에 날짜 추가

--- src/main/java/dooya/see/post/application/dto/PostResult.java
+++ src/main/java/dooya/see/post/application/dto/PostResult.java
@@ -1,9 +1,13 @@
 package dooya.see.post.application.dto;
 
+import java.time.LocalDateTime;
+
 public record PostResult(
         Long id,
         String nickName,
         String title,
-        String content
+        String content,
+        LocalDateTime createdDate,
+        LocalDateTime updatedDate
 ) {
 }
