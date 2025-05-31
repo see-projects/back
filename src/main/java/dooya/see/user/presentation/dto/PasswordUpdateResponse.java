54fdb1e6b997d5052906049146fda9a7744d1ede
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-31 19:21:07 +0900

    Response DTO에 응답 형식 작성

--- src/main/java/dooya/see/user/presentation/dto/PasswordUpdateResponse.java
+++ src/main/java/dooya/see/user/presentation/dto/PasswordUpdateResponse.java
@@ -1,5 +1,6 @@
 package dooya.see.user.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 /**
@@ -13,6 +14,8 @@ import lombok.Builder;
  */
 @Builder
 public record PasswordUpdateResponse(
+
+        @Schema(description = "업데이트 성공 메시지", example = "비밀번호가 성공적으로 변경되었습니다.")
         String message
 ) {
 }
