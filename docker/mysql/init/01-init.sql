783589399d9e1d100ab822a3c7f4898401074572
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-07-30 18:04:08 +0900

    사용자 권한 업데이트 및 미사용 charset 설정 제거
    - docker-compose와 초기화 스크립트에서 불필요한 MYSQL_CHARSET 및 COLLATION 설정 제거. 사용되지 않는 구성을 정리하여 간결화
    - MySQL 사용자 권한을 '%'에서 'localhost'로 변경하여 보안 강화

--- docker/mysql/init/01-init.sql
+++ docker/mysql/init/01-init.sql
@@ -4,7 +4,7 @@
 USE see_dev;
 
 -- 기본 사용자 권한 부여 (이미 존재하는 사용자)
-GRANT ALL PRIVILEGES ON see_dev.* TO 'see_user'@'%';
+GRANT ALL PRIVILEGES ON see_dev.* TO 'see_user'@'localhost';
 FLUSH PRIVILEGES;
 
 -- 테이블은 JPA가 자동으로 생성하므로 여기서는 기본 설정만 진행
