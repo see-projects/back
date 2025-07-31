66c8e834d646e9a5af3da7d823cbff6a07b81eeb
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-07-31 18:23:08 +0900

    사용되지 않는 import 제거

--- src/main/java/dooya/see/post/application/service/PostQueryService.java
+++ src/main/java/dooya/see/post/application/service/PostQueryService.java
@@ -4,8 +4,6 @@ import dooya.see.post.application.dto.PostResult;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface PostQueryService {
     Page<PostResult> getPosts(Pageable pageable);
     PostResult getPost(Long id);
