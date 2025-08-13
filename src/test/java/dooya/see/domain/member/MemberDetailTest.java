9af10e44b9b62b70a0a397e6da239d757a6fd7f1
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-14 01:26:58 +0900

    test(member): add test for registration date in MemberDetail
    - create 메서드로 생성된 MemberDetail의 등록일시가 설정되는지 검증하는 테스트 추가
    - 기본 생성자로 생성된 MemberDetail의 등록일시가 null인지 확인하는 기존 테스트의 이름 변경 및 수정
    - 등록일시 관련 동작 검증을 통해 MemberDetail의 생성 로직 신뢰성 강화

--- src/test/java/dooya/see/domain/member/MemberDetailTest.java
+++ src/test/java/dooya/see/domain/member/MemberDetailTest.java
@@ -7,11 +7,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberDetailTest {
     @Test
-    @DisplayName("기본 생성자로 생성된 MemberDetail은 등록일시가 null이다")
-    void memberDetailExist() {
-        MemberDetail memberDetail = new MemberDetail();
+    @DisplayName("create 메서드로 생성된 MemberDetail은 등록일시가 설정된다")
+    void memberDetailCreate() {
+        MemberDetail memberDetail = MemberDetail.create();
 
         assertThat(memberDetail).isNotNull();
         assertThat(memberDetail.getRegisteredAt()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("기본 생성자로 생성된 MemberDetail은 등록일시가 null이다")
+    void memberDetailDefaultConstructor() {
+        MemberDetail memberDetail = new MemberDetail();
+
+        assertThat(memberDetail).isNotNull();
+        assertThat(memberDetail.getRegisteredAt()).isNull();
     }
 }
