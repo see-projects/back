18ebe84b694b00d9359a0cb6086f31e02eda7615
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-14 22:30:57 +0900

    test(member): improve test case descriptions for login scenarios
    - 테스트 메서드에 @DisplayName 주석 추가함으로써 테스트 의도를 명확히 표현
    - 올바른 이메일/비밀번호, 잘못된 비밀번호, 비활성 계정의 로그인 시나리오를 각각 구분

--- src/test/java/dooya/see/application/member/provided/MemberAuthTest.java
+++ src/test/java/dooya/see/application/member/provided/MemberAuthTest.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @Import(SeeTestConfiguration.class)
 record MemberAuthTest(MemberRegister memberRegister, MemberAuth memberAuth, EntityManager entityManager) {
     @Test
-    @DisplayName("")
+    @DisplayName("올바른 이메일과 비밀번호로 로그인 시 회원 정보와 액세스 토큰을 반환한다")
     void login() {
         Member member = memberRegister.register(createMemberRegisterRequest());
         entityManager.flush();
@@ -34,8 +34,8 @@ record MemberAuthTest(MemberRegister memberRegister, MemberAuth memberAuth, Enti
     }
 
     @Test
-    @DisplayName("")
-    void passwordNotMatheLoginFail() {
+    @DisplayName("잘못된 비밀번호로 로그인 시도 시 인증 예외가 발생한다")
+    void loginFailWithWrongPassword() {
         memberRegister.register(createMemberRegisterRequest());
         entityManager.flush();
         entityManager.clear();
@@ -45,8 +45,8 @@ record MemberAuthTest(MemberRegister memberRegister, MemberAuth memberAuth, Enti
     }
 
     @Test
-    @DisplayName("")
-    void no() {
+    @DisplayName("비활성화된 계정으로 로그인 시도 시 인증 예외가 발생한다")
+    void loginFailWithDeactivatedAccount() {
         Member member = memberRegister.register(createMemberRegisterRequest());
         entityManager.flush();
         entityManager.clear();
