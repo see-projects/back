2b44ac0f57d20a6725edb9d015e0e8a87253cee4
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-14 15:27:40 +0900

    feat(member): add member registration and lookup functionality
    - 멤버 등록과 조회를 위한 인터페이스 메서드 추가
    - MemberRegister에 register 메서드, MemberFinder에 find 메서드 구현
    - 테스트 검증 추가: 등록된 멤버를 조회하여 올바르게 동작하는지 확인

--- src/main/java/dooya/see/application/member/provided/MemberRegister.java
+++ src/main/java/dooya/see/application/member/provided/MemberRegister.java
@@ -1,4 +1,8 @@
 package dooya.see.application.member.provided;
 
+import dooya.see.domain.member.Member;
+import dooya.see.domain.member.MemberRegisterRequest;
+
 public interface MemberRegister {
+    Member register(MemberRegisterRequest registerRequest);
 }
--- src/test/java/dooya/see/application/member/provided/MemberFinderTest.java
+++ src/test/java/dooya/see/application/member/provided/MemberFinderTest.java
@@ -1,13 +1,30 @@
 package dooya.see.application.member.provided;
 
 import dooya.see.SeeTestConfiguration;
+import dooya.see.domain.member.Member;
+import dooya.see.domain.member.MemberFixture;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
 @Import(SeeTestConfiguration.class)
 record MemberFinderTest(MemberFinder memberFinder, MemberRegister memberRegister, EntityManager entityManager) {
+    @Test
+    @DisplayName("")
+    void find() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+        entityManager.flush();
+        entityManager.clear();
+
+        Member found = memberFinder.find(member.getId());
+
+        assertThat(member.getId()).isEqualTo(found.getId());
+    }
 }
