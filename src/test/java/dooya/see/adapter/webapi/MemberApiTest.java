eb1dbc9deea55117e085069dcfeecf818fe6d4ca
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-14 20:24:50 +0900

    test(member-api): add initial test configuration for MemberApi
    - MemberApi 테스트를 위한 기본 설정 추가
    - SpringBootTest 및 MockMvc를 기반으로 테스트 환경 구성
    - 테스트의 일관성과 재현성을 보장하기 위해 트랜잭션 롤백 적용

---
+++ src/test/java/dooya/see/adapter/webapi/MemberApiTest.java
@@ -0,0 +1,13 @@
+package dooya.see.adapter.webapi;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@RequiredArgsConstructor
+class MemberApiTest {
+}
