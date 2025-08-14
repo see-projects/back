ad74f2ae8340f635e67e1a824c8d085e713ff894
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-14 20:29:13 +0900

    test(member): add registration API integration test
    - 회원 등록 API의 통합 테스트를 추가하여 요청/응답 검사와 성공적인 상태 코드를 보장
    - MockMvcTester 및 ObjectMapper를 활용하여 요청 JSON 생성 및 테스트 간소화
    - 기본적인 회원 등록 요청을 검증하고, 성공적인 처리(status OK)를 확인하는 테스트 구현

--- src/test/java/dooya/see/adapter/webapi/MemberApiTest.java
+++ src/test/java/dooya/see/adapter/webapi/MemberApiTest.java
@@ -1,13 +1,39 @@
 package dooya.see.adapter.webapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dooya.see.domain.member.MemberFixture;
+import dooya.see.domain.member.MemberRegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.assertj.MvcTestResult;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 @RequiredArgsConstructor
 class MemberApiTest {
+    final ObjectMapper objectMapper;
+    final MockMvcTester mvcTester;
+
+    @Test
+    @DisplayName("")
+    void register() throws JsonProcessingException {
+        MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        MvcTestResult result = mvcTester.post().uri("/api/members").contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson).exchange();
+
+        assertThat(result)
+                .hasStatusOk();
+    }
 }
