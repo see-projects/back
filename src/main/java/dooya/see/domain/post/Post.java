526aec9ff9bd351dc8cd6539909c5b69ce498df5
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-15 17:35:10 +0900

    feat(post): add PostContent value object and validation logic
    - 게시글의 제목과 내용을 관리하는 PostContent 값 객체 추가
    - 제목과 내용에 대한 필수 입력 및 길이 제한 유효성 검사 로직 구현
    - Post 엔티티에 PostContent를 포함하도록 수정
    - 관련 단위 테스트 추가로 데이터 무결성 보장

--- src/main/java/dooya/see/domain/post/Post.java
+++ src/main/java/dooya/see/domain/post/Post.java
@@ -1,4 +1,16 @@
 package dooya.see.domain.post;
 
-public class Post {
+import dooya.see.domain.AbstractEntity;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends AbstractEntity {
+    @Embedded
+    private PostContent content;
 }
