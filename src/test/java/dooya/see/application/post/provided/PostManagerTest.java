077be8ce6788ffac02c5ff63e946c29e949c513f
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-16 15:22:51 +0900

    feat(post): add unit test for PostManager's create method
    - PostManager의 create 메서드에 대한 단위 테스트 추가. 이를 통해 게시물 생성 로직이 정확히 동작하는지 검증 가능
    - 게시물 ID, 회원 ID, 상태 및 메타데이터의 유효성을 확인하는 테스트 케이스 작성
    - 테스트 안정성을 위해 PostFixture 활용 및 assertThat 사용

--- src/test/java/dooya/see/application/post/provided/PostManagerTest.java
+++ src/test/java/dooya/see/application/post/provided/PostManagerTest.java
@@ -1,12 +1,30 @@
 package dooya.see.application.post.provided;
 
 import dooya.see.SeeTestConfiguration;
+import dooya.see.domain.post.Post;
+import dooya.see.domain.post.PostStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
+import static dooya.see.domain.post.PostFixture.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
 @Import(SeeTestConfiguration.class)
-record PostManagerTest() {
+record PostManagerTest(PostManager postManager) {
+    @Test
+    @DisplayName("")
+    void a() {
+        Post post = postManager.create(createPostRequest(), 1L);
+
+        assertThat(post.getId()).isNotNull();
+        assertThat(post.getMemberId()).isEqualTo(1L);
+        assertThat(post.getStatus()).isEqualTo(PostStatus.DRAFT);
+        assertThat(post.getMetaData()).isNotNull();
+    }
 }
