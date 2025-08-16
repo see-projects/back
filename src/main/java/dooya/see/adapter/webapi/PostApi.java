c20e881c411546066d224a50513b5374b4a261c5
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-16 21:32:47 +0900

    feat(post): add API for creating posts
    - 게시글을 생성할 수 있는 새로운 API 엔드포인트 추가
    - 게시 요청 시 인증 토큰을 통해 회원 ID를 식별하고 검증
    - PostManager와 TokenManager를 활용하여 도메인 로직과 인증 분리
    - 유효성 검증을 위해 PostCreateRequest에 @Valid 적용

--- src/main/java/dooya/see/adapter/webapi/PostApi.java
+++ src/main/java/dooya/see/adapter/webapi/PostApi.java
@@ -1,9 +1,34 @@
 package dooya.see.adapter.webapi;
 
+import dooya.see.adapter.webapi.dto.PostCreateResponse;
+import dooya.see.application.member.required.TokenManager;
+import dooya.see.application.post.provided.PostManager;
+import dooya.see.domain.post.Post;
+import dooya.see.domain.post.PostCreateRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class PostApi {
+    private final PostManager postManager;
+    private final TokenManager tokenManager;
+
+    @PostMapping("/api/posts")
+    public PostCreateResponse createPost(@RequestHeader("Authorization") String token,
+                                         @RequestBody @Valid PostCreateRequest request) {
+        Long currentMemberId = getCurrentMemberId(token);
+        Post post = postManager.create(request, currentMemberId);
+
+        return PostCreateResponse.of(post);
+    }
+
+    private Long getCurrentMemberId(String token) {
+        String actualToken = token.replace("Bearer ", "");
+        return tokenManager.extractMemberIdFromToken(actualToken);
+    }
 }
