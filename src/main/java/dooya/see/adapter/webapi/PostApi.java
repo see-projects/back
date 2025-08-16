2d5d00a9041936e308fecf32950133223b700ca5
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-16 20:55:05 +0900

    feat(api): add skeleton for PostApi and its test class
    - PostApi 클래스 및 관련 테스트 클래스 생성하여 API 구조 기반 마련
    - 추후 게시글 관련 기능 구현을 위한 기본 틀 제공

---
+++ src/main/java/dooya/see/adapter/webapi/PostApi.java
@@ -0,0 +1,9 @@
+package dooya.see.adapter.webapi;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostApi {
+}
---
+++ src/test/java/dooya/see/adapter/webapi/PostApiTest.java
@@ -0,0 +1,21 @@
+package dooya.see.adapter.webapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@RequiredArgsConstructor
+class PostApiTest {
+    final ObjectMapper objectMapper;
+    final MockMvcTester mvcTester;
+
+
+}
