e86a93d1fca7677de8805db968a3e213f9e0ca72
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-08-26 16:38:29 +0900

    feat(post): enhance PostLike entity creation and validation
    - PostLike 엔티티에 Lombok 어노테이션(@Getter, @NoArgsConstructor) 추가로 코드 간결화
    - 생성 메서드 create()에 필드 유효성 검사를 추가하여 postId와 memberId에 대해 null 검증 수행
    - likedAt 속성이 생성될 때 현재 시간으로 자동 설정되도록 개선
    - 관련 테스트 코드 추가하여 PostLike 엔티티 생성 로직 검증

--- src/main/java/dooya/see/domain/post/PostLike.java
+++ src/main/java/dooya/see/domain/post/PostLike.java
@@ -2,10 +2,17 @@ package dooya.see.domain.post;
 
 import dooya.see.domain.AbstractEntity;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+import static java.util.Objects.*;
+
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostLike extends AbstractEntity {
     private Long postId;
 
@@ -13,7 +20,12 @@ public class PostLike extends AbstractEntity {
 
     private LocalDateTime likedAt;
 
-    public static PostLike create(long postId, long memberId) {
-        return null;
+    public static PostLike create(Long postId, Long memberId) {
+        PostLike postLike = new PostLike();
+        postLike.postId = requireNonNull(postId);
+        postLike.memberId = requireNonNull(memberId);
+        postLike.likedAt = LocalDateTime.now();
+
+        return postLike;
     }
 }
--- src/test/java/dooya/see/domain/post/PostLikeTest.java
+++ src/test/java/dooya/see/domain/post/PostLikeTest.java
@@ -3,10 +3,18 @@ package dooya.see.domain.post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 class PostLikeTest {
     @Test
     @DisplayName("")
     void a() {
         PostLike postLike = PostLike.create(1L, 1L);
+
+        assertNotNull(postLike);
+        assertThat(postLike.getPostId()).isNotNull();
+        assertThat(postLike.getMemberId()).isNotNull();
+        assertThat(postLike.getLikedAt()).isNotNull();
     }
 }
