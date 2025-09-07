b2457d34f77f8410ec11f33c19ca0e7aa50b4f54
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-07 22:46:00 +0900

    test(post): add CommentFixture for unit tests
    - CommentFixture 클래스 추가하여 테스트에서 사용할 댓글 생성 요청 객체(Fixture) 제공
    - 반복되는 테스트 데이터 준비 과정을 단순화하고 코드 가독성 향상

---
+++ src/test/java/dooya/see/domain/post/CommentFixture.java
@@ -0,0 +1,8 @@
+package dooya.see.domain.post;
+
+public class CommentFixture {
+
+    public static CommentCreateRequest createCommentRequest() {
+        return new CommentCreateRequest("테스트 게시물");
+    }
+}
