c07020fc4db41672a9defe0ee88e63c76fdc7bf5
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-18 20:53:13 +0900

    feat(member): rename test methods to enhance clarity
    - 테스트 메서드명을 한글로 변경하여 의도를 더 명확히 전달
    - `@DisplayName` 애너테이션 제거로 코드 간소화
    - 메서드 이름과 테스트 목적의 일치성을 개선하여 가독성과 유지보수성을 강화

--- src/test/java/dooya/see/domain/member/MemberDetailTest.java
+++ src/test/java/dooya/see/domain/member/MemberDetailTest.java
@@ -6,18 +6,16 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberDetailTest {
-    @DisplayName("create 메서드로 생성된 MemberDetail은 등록일시가 설정된다")
     @Test
-    void memberDetailCreate() {
+    void create_메서드로_생성된_MemberDetail은_등록일시가_설정된다() {
         MemberDetail memberDetail = MemberDetail.create();
 
         assertThat(memberDetail).isNotNull();
         assertThat(memberDetail.getRegisteredAt()).isNotNull();
     }
 
-    @DisplayName("기본 생성자로 생성된 MemberDetail은 등록일시가 null이다")
     @Test
-    void memberDetailDefaultConstructor() {
+    void 기본_생성자로_생성된_MemberDetail은_등록일시가_null이다() {
         MemberDetail memberDetail = new MemberDetail();
 
         assertThat(memberDetail).isNotNull();
