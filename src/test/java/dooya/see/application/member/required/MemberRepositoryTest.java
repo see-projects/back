70bec80125799e0b207eb1239a9fe7e3a3d2b95d
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-18 23:35:43 +0900

    feat(member): rename test methods and remove @DisplayName annotation
    - 테스트 메서드명을 한글로 변경하여 의도를 명확히 전달
    - `@DisplayName` 애너테이션 제거로 코드 간소화 및 중복 감소
    - 메서드 이름과 테스트 목적의 일치성을 강화하여 가독성과 유지보수성 개선

--- src/test/java/dooya/see/application/member/required/MemberRepositoryTest.java
+++ src/test/java/dooya/see/application/member/required/MemberRepositoryTest.java
@@ -3,21 +3,19 @@ package dooya.see.application.member.required;
 import dooya.see.domain.member.Member;
 import dooya.see.domain.member.MemberStatus;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import static dooya.see.domain.member.MemberFixture.createMemberRegisterRequest;
 import static dooya.see.domain.member.MemberFixture.createPasswordEncoder;
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 public record MemberRepositoryTest(MemberRepository memberRepository, EntityManager entityManager) {
-    @DisplayName("회원 생성 시 ID가 자동 생성되고 영속화 후 조회가 가능하다")
     @Test
-    void createMember() {
+    void 회원_생성_시_ID가_자동_생성되고_영속화_후_조회가_가능하다() {
         Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
 
         assertThat(member.getId()).isNull();
@@ -34,9 +32,8 @@ public record MemberRepositoryTest(MemberRepository memberRepository, EntityMana
         assertThat(found.getDetail().getRegisteredAt()).isNotNull();
     }
 
-    @DisplayName("동일한 이메일로 회원 저장 시 데이터 무결성 예외가 발생한다")
     @Test
-    void duplicateEmailFail() {
+    void 동일한_이메일로_회원_저장_시_데이터_무결성_예외가_발생한다() {
         Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
         memberRepository.save(member);
 
