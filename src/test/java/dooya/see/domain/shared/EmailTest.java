94914f7910703d8bb47535c75137e03fb99aaf37
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-18 22:30:16 +0900

    feat(shared): rename test methods and remove @DisplayName annotation
    - 테스트 메서드명을 한글로 변경하여 의도를 명확히 전달
    - `@DisplayName` 애너테이션 제거로 코드 간소화 및 중복 감소
    - 메서드 이름과 테스트 목적의 일치성을 강화하여 가독성과 유지보수성 개선

--- src/test/java/dooya/see/domain/shared/EmailTest.java
+++ src/test/java/dooya/see/domain/shared/EmailTest.java
@@ -1,40 +1,35 @@
 package dooya.see.domain.shared;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class EmailTest {
-    @DisplayName("동일한 이메일 주소를 가진 Email 객체들은 동등하다")
     @Test
-    void equality() {
+    void 동일한_이메일_주소를_가진_Email_객체들은_동등하다() {
         var email1 = new Email("dooya@see.com");
         var email2 = new Email("dooya@see.com");
 
         assertThat(email1).isEqualTo(email2);
     }
 
-    @DisplayName("이메일이 null이면 예외가 발생한다")
     @Test
-    void emailNotNull() {
+    void 이메일이_null이면_예외가_발생한다() {
         assertThatThrownBy(() -> new Email(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이메일 형식이 바르지 않습니다");
     }
 
-    @DisplayName("이메일이 빈 문자열이면 예외가 발생한다")
     @Test
-    void emailNotEmpty() {
+    void 이메일이_빈_문자열이면_예외가_발생한다() {
         assertThatThrownBy(() -> new Email(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이메일 형식이 바르지 않습니다");
     }
 
-    @DisplayName("이메일 형식이 올바르지 않으면 예외가 발생한다")
     @Test
-    void emailValid() {
+    void 이메일_형식이_올바르지_않으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Email("invalid"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이메일 형식이 바르지 않습니다");
