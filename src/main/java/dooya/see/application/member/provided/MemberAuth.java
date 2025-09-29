dc6fcdfcf3999dc6715b5b5b0c3c229270030162
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-09-29 15:51:42 +0900

    docs(member): improve method documentation in MemberAuth interface
    - MemberAuth 인터페이스 메서드의 주석을 수정하여 가독성과 이해도를 향상
    - 메서드 매개변수, 반환값, 그리고 발생 가능한 예외에 대한 정보를 자세히 기술
    - 회원 인증 관련 인터페이스의 문서화 수준을 높여 유지보수성과 사용 편의성 강화

--- src/main/java/dooya/see/application/member/provided/MemberAuth.java
+++ src/main/java/dooya/see/application/member/provided/MemberAuth.java
@@ -5,17 +5,25 @@ import dooya.see.domain.member.dto.MemberAuthRequest;
 import dooya.see.domain.member.LoginResult;
 
 /**
- * 회원 인증 Primary Port
+ * 회원 인증 기능을 제공하는 인터페이스
  */
 public interface MemberAuth {
 
     /**
-     * 회원 로그인을 수행합니다.
+     * 회원의 자격 증명을 통해 로그인 작업을 수행합니다.
+     *
+     * @param memberAuthRequest 회원 인증 요청 객체 (이메일과 비밀번호 포함)
+     * @return 로그인 결과 객체로, 회원 정보와 액세스 토큰을 포함합니다.
+     * @throws dooya.see.domain.member.exception.AuthenticateException 자격 증명이 유효하지 않거나 계정이 비활성화된 경우
      */
     LoginResult login(MemberAuthRequest memberAuthRequest);
 
     /**
-     * 액세스 토큰을 통해 현재 로그인된 회원 정보를 조회합니다.
+     * 주어진 토큰을 사용하여 현재 인증된 회원 정보를 반환합니다.
+     *
+     * @param token 인증에 사용되는 액세스 토큰
+     * @return 현재 인증된 회원 객체
+     * @throws dooya.see.domain.member.exception.AuthenticateException 유효하지 않은 토큰이거나 인증 실패 시
      */
     Member getCurrentMember(String token);
 }
