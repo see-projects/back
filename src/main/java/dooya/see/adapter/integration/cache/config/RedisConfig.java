595363fbfdc4a86a95447153449f796d2e481156
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-10-30 16:05:18 +0900

    feat(cache): enable configuration properties in Redis config
    - RedisConfig에 @EnableConfigurationProperties 추가하여 PostSearchCacheProperties 사용 가능하도록 설정
    - Redis 관련 설정 확장성을 높이고 구성 관리 효율성 강화
    - Cache 관련 속성 정의 및 유연한 설정 적용 기반 마련

--- src/main/java/dooya/see/adapter/integration/cache/config/RedisConfig.java
+++ src/main/java/dooya/see/adapter/integration/cache/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package dooya.see.adapter.integration.cache.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,6 +8,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableConfigurationProperties(PostSearchCacheProperties.class)
 public class RedisConfig {
     @Bean
     RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
